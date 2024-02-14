f2d8b1e8e99cc2ce7585f98980f54f5925bc7a7c
mrfhmd7 <mrf.hmd7@gmail.com>
2024-02-14 23:51:22 +0600

    try first server

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
      res.send("Hello from my first ever server!");
 });
 
+app.get('/data', (req, res) => {
+     res.send('more data is coming')
+});
+
 app.listen(port, () => {
      console.log(`My first server is running in port: ${port}`);
 });
